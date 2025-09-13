33badbb78fd54070e23323590a3e4b6ee6c646b4
aleja.nrj <Alejanrj.07@gmail.com>
2025-09-13 14:57:45 -0500

    (develop) inicio de validacion con expresiones regulares

---
+++ helpers/validarRegistro.js
@@ -0,0 +1,12 @@
+let inputs = document.getElementsByClassName
+(".formulario__entrada") 
+let arregloinputs = [...inputs]
+console.log(inputs)
+console.log(arregloinputs) 
+
+inputs.map((input)=> { 
+    console.log(input) 
+    console.log(input)
+    });
+
+
